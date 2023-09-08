fd3e98e6c523c5d0021f57439ab6e221487db176
Zainab omolola fadeyi <zainabfadeyi@gmail.com>
2023-09-08 10:16:20 +0100

    mu

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from datetime import datetime, timedelta
+import requests
+
+app = FastAPI()
+
+@app.get("/info/")
+async def get_info(slack_name: str, track_name: str):
+    # Validate slack_name and track_name
+    if not slack_name or not track_name:
+        raise HTTPException(status_code=400, detail="slack_name and track_name are required")
+
+    # Get the current day of the week
+    current_day_of_week = datetime.utcnow().strftime("%A")
+
+    # Get the current UTC time with validation of +/-2 hours
+    current_utc_time = datetime.utcnow()
+    utc_offset = timedelta(hours=2)  # Adjust the offset as needed
+    current_utc_time += utc_offset
+    current_utc_time_str = current_utc_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Replace these with your actual GitHub URLs
+    file_github_url = "https://github.com/your_repo/your_fie"
+    source_code_github_url = "https://github.com/your_repo/your_source_code"
+
+    # Create the response JSON
+    response_data = {
+        "slack_name": slack_name,
+        "current_day_of_week": current_day_of_week,
+        "current_utc_time": current_utc_time_str,
+        "track": track_name,
+        "file_github_url": file_github_url,
+        "source_code_github_url": source_code_github_url,
+        "status_code": "Success"
+    }
+
+    return response_data
